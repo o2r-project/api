d99831b53562f0b5f1f106f73c5cb28ea6372c2b
Fmazin <fabian.fermazin@wwu.de>
2020-09-30 17:26:32 +0200

    Fixed Theme object

--- openapi/redoc_theme.js
+++ openapi/redoc_theme.js
@@ -1,12 +1,14 @@
 Redoc.init('./openapi.yaml',{
-  colors:{
-    main: '#004286'
-  },
-  menu:{
-    backgroundColor: '#004286',
-    textColor: white
-  },
-  rightPanel:{
-    backgroundColor: '#343131'
+  theme:{
+      colors:{
+      main: '#004286'
+    },
+    menu:{
+      backgroundColor: '#004286',
+      textColor: 'white'
+    },
+    rightPanel:{
+      backgroundColor: '#343131'
+    }
   }
 }, document.getElementById('redoc-container'));
