d2cd1c784b168f0aab4fae71a719fe4b907c274c
Fmazin <48436261+Fmazin@users.noreply.github.com>
2020-09-30 17:30:41 +0200

    Update redoc_theme.js

--- openapi/redoc_theme.js
+++ openapi/redoc_theme.js
@@ -1,5 +1,4 @@
 Redoc.init('./openapi.yaml',{
-<<<<<<< HEAD
   theme:{
       colors:{
       main: '#004286'
@@ -11,16 +10,5 @@ Redoc.init('./openapi.yaml',{
     rightPanel:{
       backgroundColor: '#343131'
     }
-=======
-  colors:{
-    main: '#004286'
-  },
-  menu:{
-    backgroundColor: '#004286',
-    textColor: 'white'
-  },
-  rightPanel:{
-    backgroundColor: '#343131'
->>>>>>> f87f20453199b34d4ea45fd3e91dd055bf253a78
   }
 }, document.getElementById('redoc-container'));
