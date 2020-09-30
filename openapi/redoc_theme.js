da1584f82144841ba0c665a4610a2ff42c732c0a
Fmazin <fabian.fermazin@wwu.de>
2020-09-30 17:20:36 +0200

    Added options object to redoc

---
+++ openapi/redoc_theme.js
@@ -0,0 +1,12 @@
+Redoc.init('./openapi.yaml',{
+  colors:{
+    main: '#004286'
+  },
+  menu:{
+    backgroundColor: '#004286',
+    textColor: white
+  },
+  rightPanel:{
+    backgroundColor: '#343131'
+  }
+}, document.getElementById('redoc-container'));
